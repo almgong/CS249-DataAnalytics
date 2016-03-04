093c7bdfe4bbeba056c2fff71112cc70c9c1ecc1
Allen G <alm.gong@gmail.com>
2016-03-04 15:46:38 -0800

    update src structure

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 from pyspark.mllib.classification import LogisticRegressionWithLBFGS, LogisticRegressionModel
 from pyspark.mllib.regression import LabeledPoint
 from pyspark.mllib.clustering import *
+from pyspark.mllib.linalg import SparseVector
 
 ###preprocess###
-from preprocesser import Dataformatter
-from trendfinder import Trends 
+#from preprocesser import Dataformatter
+#from trendfinder import Trends 
 
 '''
 Main driver for CS249 classification problem.
 Designed to run in the interactive pyspark shell.
 '''
 
-### Code to obtain data ###
-print "Obtaining and Formatting data..."
-
-
-### Run preprocessing ###
+### Run preprocessing ### NOT USED
 print "Preprocessing..."
+'''
 userDict = {}
 userFV = {} 	#feature vectors per user
 Dataformatter.parseKDDData(userDict,
@@ -31,17 +29,39 @@ userFV = [userFV[i] for i in userFV]	#convert to list of SparseVectors()
 bcFV = sc.broadcast(userFV)
 userFV = None
 model = KMeans.train(sc.parallelize(bcFV.value), 20, initializationMode="k-means||",seed=50, initializationSteps=5, epsilon=1e-4)
+'''
 
 """t = Trends.numFollowersBasedOnBirthYear(f)
 t = Trends.numDistinctTagIds(f)
 t = Trends.numDistinctKeywords(f)"""
 
-### Run main algorithm ###
-#lrm = LogisticRegressionWithLBFGS.train(sc.parallelize(data), iterations=10)
 
-### Post Processing ###
-print "Postprocessing..."
 
-### Final output ###
-print "Predicting..."
-#lrm.predict([.5,.4]) #should be 1
+'''
+	Each algorithm will output its own list of items with ratings greater than
+	some lower bound, then as input to LR, we need to automatically fill in
+	gaps with some default low number (0.1)
+
+'''
+
+
+
+### Any preprocessing needed (may be none)
+print "Preprocessing - Preparing data"
+
+
+
+### Main logic, run 3 sub algorithms ###
+print "Runing main logic"
+
+
+
+### Postprocessing - may be none ###
+print "Postprocessing - Cleaning up"
+
+
+
+
+
+
+
