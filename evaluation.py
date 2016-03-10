0e8b1e8aef3643120e01ccd5a57a7bb4cb9e87f2
chenyangxiuqi <chenyangxiuqi@gmail.com>
2016-03-09 19:37:20 -0800

    Added files via upload

--- evaluation.py
+++ evaluation.py
@@ -16,7 +16,8 @@ def precision_compute(stat, aver_precision):
         if stat[i] != 0:
             top_prec += float(sum[i]) / (i+1)
                     
-    aver_precision[0] += top_prec / sum[-1]   # the sum of precision
+    if sum[-1] > 0:
+        aver_precision[0] += top_prec / sum[-1]   # the sum of precision
     
 def readRecom(filename, user):
     with open(filename) as f:
@@ -29,14 +30,14 @@ def readRecom(filename, user):
                 
                 
                 
-def readTraining(filename, user):
+def readSolution(filename, user):
     with open(filename) as f:
         for line in f:
             curr = line.split(',')
             click_item = curr[1].split()   # obtain the list of click item
             
             for ele in set(user[curr[0]]['recommendation']) & set(click_item):
-                user[curr[0]]['stat'][user[curr[0]['recommendation'].index(ele)] = 1
+                user[curr[0]]['stat'][user[curr[0]]['recommondation'].index(ele)] = 1
             
                     
                     
@@ -45,5 +46,13 @@ def stat_compute(user, aver_precision):
         precision_compute(user[userId]['stat'], aver_precision)
         
 
-user = {}
+
+def evaluation(filename1, filename2):   # wrapped function
+    user = {}
+    readRecom(filename1, user)
+    readSolution(filename2, user)
+    
+    aver_precision = [0, 0]
+    stat_compute(user, aver_precision)
+    return aver_precision[0] / aver_precision[1]
         
