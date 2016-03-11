2c3e9da9af737ae79d072c7bcd8073890ad6e658
Han Wang <hanwang@s-164-67-237-162.resnet.ucla.edu>
2016-03-11 10:19:48 -0800

    save user-item-rating to file without return

--- subalg/user_item/user_item.py
+++ subalg/user_item/user_item.py
@@ -6,10 +6,10 @@ from pyspark.mllib.linalg import Vectors
 from pyspark.mllib.feature import Normalizer
 
 # train to build CF model with full info (user, product, following)
-file_train1 = "CS249_DataAnalytics/part_00000.txt"
+file_train1 = "subalg/user_item/train1.txt"
 
 # predict result of train data (user, product) with model
-file_train2 = "CS249_DataAnalytics/part_00001.txt"
+file_train2 = "data/train2.txt"
 
 def generateCandidatesWithWeights(sc):
 	# load training and test data into (user, product, rating) tuples
@@ -28,5 +28,4 @@ def generateCandidatesWithWeights(sc):
 	test_data = test.map(lambda x: (x[0], x[1]))
 	#predictions are the input for logistic regression model
 	predictions = model.predictAll(test_data).map(lambda r: (r[0],r[1],r[2]))
-	predictions.coalesce(1, True).saveAsTextFile("CS249_DataAnalytics/subalg/user_item/prediction.txt")
-	return predictions
+	predictions.coalesce(1, True).saveAsTextFile("subalg/user_item/output/user_item_results.txt")
