430d3e68a3ad49cbb4e72f6abdbcaf3f209b732b
Allen G <alm.gong@gmail.com>
2016-03-11 12:06:38 -0800

    remove comma...

--- splitTrainFile.py
+++ splitTrainFile.py
@@ -3,7 +3,7 @@ from pyspark.mllib.evaluation import BinaryClassificationMetrics
  
 # load training and test data into (user, product, rating) tuples
 def parseRating(line):
-	fields = line.split(',')	
+	fields = line.split()	
 	return (int(fields[0]), int(fields[1]), float(fields[2]))   
 data = sc.textFile("data/rec_log_train.txt").map(parseRating).cache()
 file1, file2, file3 = data.randomSplit([0.3, 0.5, 0.2])
