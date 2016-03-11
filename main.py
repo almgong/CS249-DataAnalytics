2edaef619f481f12f8093c2dea28bc167a5e6900
Allen G <alm.gong@gmail.com>
2016-03-11 14:37:34 -0800

    main.py update to include splitFile

--- main.py
+++ main.py
@@ -10,50 +10,18 @@ import evaluation
 
 import gc
 import os #for deleting temp files
+import time
 
 '''
 Main driver for CS249 classification problem.
 Designed to run in the interactive pyspark shell.
 '''
-
-### Run preprocessing ### NOT USED
-'''
-userDict = {}
-userFV = {} 	#feature vectors per user
-Dataformatter.parseKDDData(userDict,
-	userFV, 
-	"data/user_profile.txt", 
-	"data/item.txt",
-	"data/user_sns.txt",
-	"data/user_key_word.txt")
-
-userDict = None 						#null to free up memory
-userFV = [userFV[i] for i in userFV]	#convert to list of SparseVectors()
-bcFV = sc.broadcast(userFV)
-userFV = None
-model = KMeans.train(sc.parallelize(bcFV.value), 20, initializationMode="k-means||",seed=50, initializationSteps=5, epsilon=1e-4)
-'''
-
-"""t = Trends.numFollowersBasedOnBirthYear(f)
-t = Trends.numDistinctTagIds(f)
-t = Trends.numDistinctKeywords(f)"""
-
-
-
-'''
-	Each algorithm will output its own list of items with ratings greater than
-	some lower bound, then as input to LR, we need to automatically fill in
-	gaps with some default low number (0.1)
-
-'''
-
-
-################################## new ################################
-
+start = time.time()
 
 ### Any preprocessing needed (may be none)
 print "Preprocessing..."
-
+print "Splitting training file..."
+execfile('splitTrainFile.py')
 
 
 ### Main logic, run 3 sub algorithms ###
