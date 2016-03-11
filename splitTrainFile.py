e07f923c05753ccf30c8e8dd7ee40f1b564d62cb
Han Wang <hanwang@s-164-67-237-162.resnet.ucla.edu>
2016-03-11 15:13:16 -0800

    modified

--- splitTrainFile.py
+++ splitTrainFile.py
@@ -1,12 +1,9 @@
-from pyspark.mllib.recommendation import ALS
-from pyspark.mllib.evaluation import BinaryClassificationMetrics
- 
 # load training and test data into (user, product, rating) tuples
 def parseRating(line):
 	fields = line.split()	
 	return (int(fields[0]), int(fields[1]), float(fields[2]))   
-data = sc.textFile("data/rec_log_train.txt").map(parseRating).cache()
+data = sc.textFile("/Users/hanwang/CS249_DataAnalytics/data/rec_log_train.txt").map(parseRating).cache()
 file1, file2, file3 = data.randomSplit([0.3, 0.5, 0.2])
-file1.coalesce(1, True).saveAsTextFile("data/train1.txt")
-file2.coalesce(1, True).saveAsTextFile("data/train2.txt")
-file3.coalesce(1, True).saveAsTextFile("data/train3.txt")
+file1.coalesce(1, True).saveAsTextFile("/Users/hanwang/CS249_DataAnalytics/data/train1")
+file2.coalesce(1, True).saveAsTextFile("/Users/hanwang/CS249_DataAnalytics/data/train2")
+file3.coalesce(1, True).saveAsTextFile("/Users/hanwang/CS249_DataAnalytics/data/train3")
