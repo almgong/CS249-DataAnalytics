fa3e00c7f0415a83165136cd1da9ac5b23db9f3f
Han Wang <hanwang@s-164-67-237-162.resnet.ucla.edu>
2016-03-11 17:36:48 -0800

    change the split rate

--- splitTrainFile.py
+++ splitTrainFile.py
@@ -2,8 +2,8 @@
 def parseRating(line):
 	fields = line.split()	
 	return (int(fields[0]), int(fields[1]), float(fields[2]))   
-data = sc.textFile("/Users/hanwang/CS249_DataAnalytics/data/rec_log_train.txt").map(parseRating).cache()
-file1, file2, file3 = data.randomSplit([0.3, 0.5, 0.2])
-file1.coalesce(1, True).saveAsTextFile("/Users/hanwang/CS249_DataAnalytics/data/train1")
-file2.coalesce(1, True).saveAsTextFile("/Users/hanwang/CS249_DataAnalytics/data/train2")
-file3.coalesce(1, True).saveAsTextFile("/Users/hanwang/CS249_DataAnalytics/data/train3")
+data = sc.textFile("data/rec_log_train.txt").map(parseRating).cache()
+file1, file2, file3 = data.randomSplit([0.1, 0.5, 0.4])
+file1.coalesce(1, True).saveAsTextFile("data/train1")
+file2.coalesce(1, True).saveAsTextFile("data/train2")
+file3.coalesce(1, True).saveAsTextFile("data/train3")
