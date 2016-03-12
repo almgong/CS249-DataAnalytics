31724ead94388a04030cc83f272ca1c541e0bcdd
Han Wang <hanwang@s-164-67-237-162.resnet.ucla.edu>
2016-03-11 23:38:42 -0800

    modified

--- subalg/user_item/user_item.py
+++ subalg/user_item/user_item.py
@@ -11,8 +11,6 @@ file_train1 = "data/train1.txt"
 # predict result of train data (user, product) with model
 file_train2 = "data/train2.txt"
 
-file_test = "data/rec_log_test.txt"
-
 def generateCandidatesWithWeights(sc):
 	# load training and test data into (user, product, rating) tuples
 	def parseRating(line):
@@ -20,24 +18,15 @@ def generateCandidatesWithWeights(sc):
 		fields = fields.rstrip(')')
 		fields = fields.split(',')
 		return (int(fields[0]), int(fields[1]), (float(fields[2]) + 1)/2)
-
-	def parseRating2(line):
-		fields = line.split()
-		return (int(fields[0]), int(fields[1]), (float(fields[2]) + 1)/2)
 		   
 	train1 = sc.textFile(file_train1).map(parseRating).cache()
 	# test data for CF model part but train data for later logistic regression model
 	test = sc.textFile(file_train2).map(parseRating).cache()
 
 	# train a recommendation model
-	model = ALS.train(train1, rank = 10, iterations = 3)
+	model = ALS.train(train1, rank = 10, iterations = 2)
 	# make predictions on (user, product) pairs from the test data
 	test_data = test.map(lambda x: (x[0], x[1]))
 	#predictions are the input for logistic regression model
 	predictions = model.predictAll(test_data).map(lambda r: (r[0],r[1],r[2]))
 	predictions.coalesce(1, True).saveAsTextFile("subalg/user_item/output")
-
-	filetest = sc.textFile(file_test).map(parseRating2).cache()
-	filetest_data = filetest.map(lambda x: (x[0], x[1]))
-	predictions_test = model.predictAll(filetest_data).map(lambda r: (r[0],r[1],r[2]))
-	predictions_test.coalesce(1, True).saveAsTextFile("subalg/user_item/test")
